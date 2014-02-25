ce27a795d8a55a5efa946595b011a83f8aa69202
Enrique Barbeito García <ebarbeito@gmail.com>
2014-02-25 17:29:45 +0100

    job token is now autogenerated when creating new jobs

--- src/ebarbeito/JobeetBundle/Entity/Job.php
+++ src/ebarbeito/JobeetBundle/Entity/Job.php
@@ -476,6 +476,15 @@ class Job {
   }
 
   /**
+   * @ORM\PrePersist
+   */
+  public function setTokenValue() {
+    if (!$this->getToken()) {
+      $this->token = sha1($this->getEmail() . rand(11111, 99999));
+    }
+  }
+
+  /**
    * @ORM\PreUpdate
    */
   public function setUpdatedAtValue() {
